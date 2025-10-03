4c37993c156c197c863f9ab59d3f4b76803bcbbd
Damanjeet Singh <damanjeetsingh434@gmail.com>
2025-10-04 00:36:32 +0530

    added data presistence
    Signed-off-by: Damanjeet Singh <damanjeetsingh434@gmail.com>

--- src/provider/query.provider.tsx
+++ src/provider/query.provider.tsx
@@ -1,10 +1,49 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactNode } from "react";
 
-const queryClient = new QueryClient();
+import { persistQueryClient } from "@tanstack/react-query-persist-client";
+
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+
+import React, { ReactNode, useEffect, useState } from "react";
 
 export function QueryProvider({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 15,
+            refetchOnWindowFocus: false,
+            refetchOnMount: false,
+          },
+        },
+      })
+  );
+
+  const [isHydrated, setIsHydrated] = useState(false);
+
+  useEffect(() => {
+    const enablePersistence = async () => {
+      const localStoragePersister = createSyncStoragePersister({
+        storage: window.localStorage,
+        key: "react-query",
+      });
+
+      await persistQueryClient({
+        queryClient,
+        persister: localStoragePersister,
+        maxAge: 1000 * 60 * 15,
+      });
+
+      setIsHydrated(true);
+    };
+
+    enablePersistence();
+  }, [queryClient]);
+
+  if (!isHydrated) return null;
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
