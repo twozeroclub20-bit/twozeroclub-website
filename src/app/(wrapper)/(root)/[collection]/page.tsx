9d8a7771a89c693edca2e9b1a2119e7003d1f756
Damanjeet Singh <damanjeetsingh434@gmail.com>
2025-12-07 22:11:52 +0530

    temp fix
    Signed-off-by: Damanjeet Singh <damanjeetsingh434@gmail.com>

--- src/app/(wrapper)/(root)/[collection]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-import React from "react";
-
-export default function page({ params }: { params: { collection: string } }) {
-  redirect(`/${params.collection}/all`);
-}
